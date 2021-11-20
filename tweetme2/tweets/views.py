3407d77a7bf002563095f18f7b190bbe37a9841d
ishwar6 <iisudrj11@gmail.com>
2021-11-20 21:52:23 +0530

    Tweet View

--- tweetme2/tweets/views.py
+++ tweetme2/tweets/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def home_view(request, *args, **kwargs):
+    print(args, kwargs)
+    return HttpResponse("<h1>Hey</h1>")
+
+
+def tweet_detail_view(request, tweet_id):
+    return HttpResponse(f"<h1>Hello {tweet_id}</h1>")
