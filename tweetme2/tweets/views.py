3263496913c972d310b6c0a4640ea58e9b55d12c
ishwar6 <iisudrj11@gmail.com>
2021-11-20 22:02:00 +0530

    REST API

--- tweetme2/tweets/views.py
+++ tweetme2/tweets/views.py
@@ -1,5 +1,7 @@
+from django.http.response import Http404
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from .models import Tweet
 
 
 def home_view(request, *args, **kwargs):
@@ -8,4 +10,17 @@ def home_view(request, *args, **kwargs):
 
 
 def tweet_detail_view(request, tweet_id):
-    return HttpResponse(f"<h1>Hello {tweet_id}</h1>")
+    data = {
+        "id": tweet_id,
+
+    }
+    try:
+        tweet_obj = Tweet.objects.get(id=tweet_id)
+        data['content'] = tweet_obj.content
+        status = 200
+    except:
+        data['message'] = "Tweet Not Found"
+        status = 404
+
+    # return HttpResponse(f"<h1>Hello {tweet_id} - {tweet_obj.content}</h1>")
+    return JsonResponse(data, status=status)
