f07800ded278c42bab61dbd598d3a99e3ab83b44
ishwar6 <iisudrj11@gmail.com>
2021-11-23 12:20:03 +0530

    Strings

--- basic.py
+++ basic.py
@@ -1,4 +1,5 @@
-"""
+
+from timeit import default_timer as timer
 mydict = {"name": "max", "age": 28, "city": "New york"}
 print(mydict)
 
@@ -37,7 +38,7 @@ mydict_copy_new["name"] = "Sri Krishna"
 print(mydict)
 
 new_dict = dict(name="Mahipal", age=21)
-mydict.update(new_dict) #overwrites old dict with new dict data
+mydict.update(new_dict)  # overwrites old dict with new dict data
 print(mydict)
 
 # a tuple can be a key to dictionary, but not a list because list is mutable and can be chaged later on
@@ -46,11 +47,12 @@ mydict = {mytuple: 12}
 print(mydict)
 
 A set in python is a data structure that contains an unordered collection of unique and immutable objects.
-Set in Python is a data structure equivalent to sets in mathematics. 
+Set in Python is a data structure equivalent to sets in mathematics.
 
-It may consist of various elements; the order of elements in a set is undefined. You can add and delete elements of a set, you can iterate the elements of the set, 
+It may consist of various elements
+the order of elements in a set is undefined. You can add and delete elements of a set, you can iterate the elements of the set,
 
-you can perform standard operations on sets (union, intersection, difference).
+you can perform standard operations on sets(union, intersection, difference).
 Besides that, you can check if an element belongs to a set.
 
 Though sets can’t contain mutable objects, sets are mutable. But since they are unordered, indexing have no meaning. We cannot access or change an element of set using indexing or slicing. Set does not support it.
@@ -114,10 +116,10 @@ print(evens)
 print(primes)  # {2}
 
 primes.difference_update(evens)
-print(primes) 
+print(primes)
 
 
-
+"""
 Q. What's the difference between these two set methods?
 
 A. The update version subtracts from an existing set, mutating it, and potentially leaving it smaller than it originally was. The non-update version produces a new set, leaving the originals unchanged.
@@ -136,7 +138,7 @@ If the size of s is four or more times larger that t, the new set version first 
 
 Otherwise, the algorithm for the non-updating version is to create an empty new set n, loop over the elements of s and add them to n if they are not present in t.
 
-
+"""
 setA = {3, 4, 5, 6, 7, 8}
 setB = {3, 4, 1, 2}
 setC = {3}
@@ -162,16 +164,137 @@ set_copyA2 = setA.copy()
 # it is frozen, no changes allowed, no add or remove can be made here
 a = frozenset([2, 3, 4])
 print(a)
-#a.add(33) AttributeError: 'frozenset' object has no attribute 'add'
+# a.add(33) AttributeError: 'frozenset' object has no attribute 'add'
 
-#union, update and difference methods works on frozenset
+# union, update and difference methods works on frozenset
 
 
-#set is mutable but with immutable elements. 
+# set is mutable but with immutable elements.
 setA = {2, [33, 3]}
 TypeError: unhashable type: 'list'
 
 for letter in set(“apple”):
     print(letter)
-#p l e a
-"""
+# p l e a
+
+
+my_string = """hi
+hey this is multiple 
+line string"""
+
+
+print(my_string)
+
+my_string = """hi\
+hey this is multiple\
+
+line string"""
+print(my_string)
+
+print(my_string[0])
+
+#my_string[0] = "H"
+# TypeError: 'str' object does not support item assignment
+# strings are ordered, immutable, text representation
+
+print(my_string[1:5])  # index 5 is excluded
+
+# use + for concatanation
+# TO LOOP OVER CHARACTERS
+for i in my_string:
+    # print(i)
+    pass
+
+# TO check
+if "l" in my_string:
+    print("YES")
+
+my_string = '    here is lot of white   space     '
+print(my_string)
+my_string = my_string.strip()
+print(my_string)  # removes leading and trailing whitespace
+# doen't change string in place because string is immutable
+
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.startswith("U"))
+print(my_string.endswith("e"))
+print(my_string.find('o'))  # return index starting from 0
+print(my_string.find("P"))  # return -1 if not present
+print(my_string.count('o'))  # counts charaters present in total
+# if pattern to replace don't find, it don't change
+print(my_string.replace("lot of a", "NO"))
+print(my_string.replace("lot of", "NO"))
+
+# here is lot of white   space
+list_from_string = my_string.split()
+print(list_from_string)  # delimiter used by default is space
+#['here', 'is', 'lot', 'of', 'white', 'space']
+
+list_from_string = my_string.split(",")
+print(list_from_string)
+#['here is lot of white   space']
+
+#list_from_string = my_string.split("")
+print(list_from_string)
+# ValueError: empty separator
+
+string_from_list = "".join(my_string)
+print(string_from_list)  # here is lot of white   space
+# ['here', 'is', 'lot', 'of', 'white', 'space']
+list_from_string = my_string.split()
+string_from_list = "".join(list_from_string)
+print(string_from_list)  # hereislotofwhitespace
+
+string_from_list = ".".join(list_from_string)  # puts . between each element
+# here.is.lot.of.white.space
+print(string_from_list)
+string_from_list = ".".join(my_string)  # if you pass string in place of list
+# h.e.r.e. .i.s. .l.o.t. .o.f. .w.h.i.t.e. . . .s.p.a.c.e
+
+
+string_from_list = " ".join(list_from_string)  # puts . between each element
+# JOIN: Concatenate any number of strings. The string whose method is called is inserted in between each given string. The result is returned as a new string.
+
+print(string_from_list)  # here is lot of white space
+
+my_list = ['a'] * 40
+# print(my_list)
+
+start = timer()
+# a bad and expensive method to create string from this list
+new_string = ""
+for i in my_list:
+    new_string += i
+stop = timer()
+print(stop-start)
+# print(new_string)
+
+# convert list ['a', 'a', 'a', 'a'] into string : aaaa
+# because string is immutable, it every time creates a new string for concatanation
+# hence join method is used.
+start = timer()
+new_string = "".join(my_list)
+stop = timer()
+print(stop-start)
+# print(new_string)  # aaaa
+
+# FORMATTING A STRING
+# by %, format() or f-string
+
+var = "Tom"
+my_string = "the variable is %s" % var
+print(my_string)
+# use %f for float, %.2f (2 digit after float)
+
+var = 3.44
+my_string = "the variable is {}".format(var)
+print(my_string)
+
+var1 = "a"
+var2 = 32
+my_string = "the variable is {}, {}".format(var1, var2)
+print(my_string)
+my_string = f"the variable is {var1} and {var2}"
+my_string = f"the variable is {var1*22} and {var2}"
+print(my_string)
