5afe7bca6e177a5c68dd38e05190357305ee113b
ishwar6 <iisudrj11@gmail.com>
2021-11-23 22:34:28 +0530

    Final till boot

--- tweetme2/tweets/models.py
+++ tweetme2/tweets/models.py
@@ -1,6 +1,10 @@
 from django.db import models
+from django.conf import settings
+
+User = settings.AUTH_USER_MODEL
 
 
 class Tweet(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  # models.SET_NULL
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to="", blank=True)
