04cf8a1021d6dff625f33bdec69f4a770b24d7e7
ishwar6 <iisudrj11@gmail.com>
2021-11-27 11:30:05 +0530

    Insertion in list

--- tweetme2/ds.py
+++ tweetme2/ds.py
@@ -2,6 +2,7 @@
 
 class Node:
     def __init__(self, data=None, next=None):
+        print("Running for", data)
         self.data = data
         self.next = None
 
@@ -11,9 +12,43 @@ class LinkedList:
 
         self.head = None
 
+    def printList(self):
+        temp = self.head
+        while(temp):
+            print(temp.data, "-- ->", end=" ")
+            temp = temp.next
+
+    def insert_at_beg(self, data):
+        node = Node(data)
+        if self.head == None:
+            self.head = node
+        else:
+            temp = self.head
+            node.next = temp
+            self.head = node
+
+    def insert_at_end(self, data):
+        node = Node(data)
+        if self.head == None:
+            self.head = node
+        else:
+            temp = self.head
+            while temp.next:
+                temp = temp.next
+            temp.next = node
+
 
 llist = LinkedList()
 
 llist.head = Node(1)
-llist.head = Node(2)
+second = Node(2)
+third = Node(3)
+llist.head.next = second
+second.next = third
+
 print(llist.head)
+llist.insert_at_beg(22)
+llist.insert_at_end(123)
+llist.insert_at_beg(11)
+llist.insert_at_end(33)
+llist.printList()
