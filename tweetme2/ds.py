dd7f604d1e425c84ece6d76aff7d693ca78378ce
ishwar6 <iisudrj11@gmail.com>
2021-11-27 14:25:04 +0530

    Deletion

--- tweetme2/ds.py
+++ tweetme2/ds.py
@@ -65,18 +65,57 @@ class LinkedList:
         else:
             print("EMPTY")
 
+    def delete_x_position(self, x: int):
+        print("To be deleted", x, "position")
+        l = self.length_of_list()
+        if x > l:
+            print("position do not exists")
+        else:
+            if x == 1:
+                print('deleting first node')
+                if self.head.next:
+                    self.head = self.head.next
+                    return
+                else:
+                    self.head = None
+                    return
+
+            temp = self.head
+
+            while x > 2:
+                temp = temp.next
+                x -= 1
+
+            if temp.next and temp.next.next:
+                temp.next = temp.next.next
+            elif temp.next:
+                temp.next = None
+            else:
+
+                self.head = None
+
+    def length_of_list(self):
+        l = 0
+        if self.head is None:
+            pass
+        else:
+            temp = self.head
+            while temp:
+                l += 1
+                temp = temp.next
+        return l
+        print("Length of list is", l)
+
 
 llist = LinkedList()
 llist.insert_at_beg(1)
-llist.delete_end()
 llist.insert_at_end(2)
-llist.delete_from_beg()
-llist.delete_from_beg()
-llist.delete_from_beg()
 llist.insert_at_end(3)
-llist.delete_from_beg()
 llist.insert_at_beg(0)
-llist.delete_end()
+llist.insert_at_beg(-1)
+llist.length_of_list()
+llist.delete_x_position(3)
+
 
 print("Final")
 llist.printList()
