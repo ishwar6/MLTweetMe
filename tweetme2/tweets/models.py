36910bb026c6106e66d466a1b163760b7d9d5b10
ishwar6 <iisudrj11@gmail.com>
2021-11-30 20:21:15 +0530

    ManytoMany

--- tweetme2/tweets/models.py
+++ tweetme2/tweets/models.py
@@ -5,6 +5,33 @@ User = settings.AUTH_USER_MODEL
 
 
 class Tweet(models.Model):
+    TYPES = (
+        ('U', "User"),
+        ("A", "Administrator")
+    )
     user = models.ForeignKey(User, on_delete=models.CASCADE)  # models.SET_NULL
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to="", blank=True)
+    user_type = models.CharField(choices=TYPES, max_length=1)
+
+
+class Person(models.Model):
+    name = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.name
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=128)
+    members = models.ManyToManyField(Person, through='Membership')
+
+    def __str__(self):
+        return self.name
+
+
+class Membership(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    date_joined = models.DateField()
+    invite_reason = models.CharField(max_length=64)
