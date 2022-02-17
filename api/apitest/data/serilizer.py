ad315ee06515e5c6e2b232da9a6c543a7d639ab2
ishwar6 <iisudrj11@gmail.com>
2022-02-17 11:14:52 +0530

    try relations

--- api/apitest/data/serilizer.py
+++ api/apitest/data/serilizer.py
@@ -7,3 +7,9 @@ class PersonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Person
         fields = ('name', 'birth_year')
+
+
+class StudentSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=100)
+    roll = serializers.IntegerField(null=True, blank=True)
+    city = serializers.CharField(max_length=100)
--- tweetme2/runbasic.py
+++ tweetme2/runbasic.py
@@ -28,6 +28,10 @@
 
 
 import json
-python_data = {"name": "Hero", "age": 11}
+python_data = {'name': "Hero", "age": 11, "a": 2}
 json_data = json.dumps(python_data)
 print(json_data)
+print(type(json_data))
+p = json.loads(json_data)
+print(p)
+print(type(p))
